a1e268961241f82b7b11afcea33b5a4a773f1459
dhvani vora <dhvani.vora2016@gmail.com>
2020-11-28 15:59:26 +0530

    Changed the folder name

--- functions/generator.py
+++
@@ -1,8 +0,0 @@
-# The generator function
-def Fun(): 
-    yield 1            
-    yield 2            
-    yield 3            
-
-for value in Fun():  
-    print(value) 
--- functions/odd_even.py
+++
@@ -1,9 +0,0 @@
-
-# A function to check whether x is even or odd 
-def OddEven( x ): 
-    if (x % 2 == 0): 
-        print "even"
-    else: 
-        print "odd"
-  
-OddEven(211)
